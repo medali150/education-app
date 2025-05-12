ce0c2919def0740cdeb349b9b0155a8d2785d4a7
medali150 <mohamedali.gh15@gmail.com>
2025-05-12 16:17:49 +0100

    first commit

---
+++ favoris-service/src/test/java/com/formations/favoris_service/FavorisServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.formations.favoris_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = com.formations.favoris_service.FavorisServiceApplication.class)
+class FavorisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
